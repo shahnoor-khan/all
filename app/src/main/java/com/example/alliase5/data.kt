1a4785d393a16d00b675f49ea2c9c474d3fcbf49
shahnoor-khan <shanoor2040@gmail.com>
2021-11-13 23:54:15 +0530

    hiiii

--- app/src/main/java/com/example/alliase5/data.kt
+++ app/src/main/java/com/example/alliase5/data.kt
@@ -1,8 +1,9 @@
 package com.example.alliase5
 
-data class data (val name:String?,val father:String?,val mother:String?,val age:String?,val gender:String,val bio:String?){
+data class a (val name:String?,
+              val father:String?,
+              val mother:String?,
+              val age:String?,
+              val gender:String?,
+              val bio:String?)
 
-
-
-
-}
--- app/src/main/java/com/example/alliase5/home1.kt
+++ app/src/main/java/com/example/alliase5/home1.kt
@@ -1,34 +1,42 @@
 package com.example.alliase5
 
+import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class home1 : Fragment(),NavigationView.OnNavigationItemSelectedListener{
 
-/**
- * A simple [Fragment] subclass.
- * Use the [home1.newInstance] factory method to
- * create an instance of this fragment.
- */
-class home1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var navController: NavController
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var navcon:NavController
+    data class a (val name:String?="",
+                  val father:String?="",
+                  val mother:String?="",
+                  val age:String?="",
+                  val gender:String?="",
+                  val bio:String?="")
+
+    class ViewHolder (view: View):RecyclerView.ViewHolder(view){
+        val name: TextView = view.findViewById(R.id.name2)
+        val father: TextView = view.findViewById(R.id.father)
+        val mother : TextView = view.findViewById(R.id.mother2)
+        val age: TextView = view.findViewById(R.id.age1)
+        val gen: TextView =view.findViewById(R.id.gender2)
+        val bio: TextView = view.findViewById(R.id.bio1)
     }
 
     override fun onCreateView(
@@ -39,33 +47,103 @@ class home1 : Fragment() {
         return inflater.inflate(R.layout.fragment_home1, container, false)
       }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val dataa = datasource().load()
-        val recy = view.findViewById<RecyclerView>(R.id.rey)
-        Adapter(dataa ,this).also { recy.adapter = it }
-        recy.setHasFixedSize(true)
+        setHasOptionsMenu(true)
+        val na:NavigationView = view.findViewById(R.id.navi)
+        val menu: Menu =na.menu
+        val navcon:NavController= Navigation.findNavController(view)
+        val draw:DrawerLayout=view.findViewById(R.id.drawer)
+        val tool =view.findViewById<Toolbar>(R.id.toolbar)
+        tool.setNavigationIcon(R.drawable.ic_baseline_menu_24)
+        tool.setTitle("Alliase")
+        tool.inflateMenu(R.menu.tool)
+        tool.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.not ->{
+                    navcon.navigate(R.id.action_home1_to_notification)
+                    Log.d("tag","sucess")
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.rec ->{
+                    navcon.navigate(R.id.action_home1_to_recent)
+                    return@setOnMenuItemClickListener true
+                }
 
-    }
+                else -> return@setOnMenuItemClickListener false
+            }
+        }
+        menu.setGroupCheckable(R.id.grou,true,true)
 
+        tool.setNavigationOnClickListener {
+            na.setupWithNavController(navcon)
+            draw.openDrawer(na)
+            na.setNavigationItemSelectedListener {
+                    when (it.itemId) {
+                        R.id.profile -> {
+                            navcon.navigate(R.id.action_home1_to_profile)
+                            Log.d("tag","sucess")
+                            return@setNavigationItemSelectedListener true
+                        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment home1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            home1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                        R.id.apperance -> {
+                            navcon.navigate(R.id.action_home1_to_apperance2)
+                            return@setNavigationItemSelectedListener true
+                        }
+                        else -> return@setNavigationItemSelectedListener true
+                    }
                 }
             }
+
+
+
+
+        val db = Firebase.firestore
+        val qu = db.collection("data")
+        val g = FirestoreRecyclerOptions.Builder<a>().setQuery(qu,a::class.java).setLifecycleOwner(this).build()
+        val adapt = object : FirestoreRecyclerAdapter<a,ViewHolder>(g){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
+                val layout = LayoutInflater.from(parent.context).inflate(R.layout.card,parent,false)
+                return ViewHolder(layout)
+            }
+
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int, model: a) {
+                holder.name.text = model.name
+                holder.mother.text = model.mother
+                holder.father.text = model.father
+                holder.age.text = model.age
+                holder.bio.text = model.bio
+                holder.gen.text= model.gender
+            }
+
+        }
+        val recy = view.findViewById<RecyclerView>(R.id.rey)
+        recy.adapter = adapt
+        recy.layoutManager=LinearLayoutManager(context)
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        navcon = Navigation.findNavController(
+            activity?.supportFragmentManager?.findFragmentById(R.id.home1)!!.requireView())
+        Log.d("tag","sucess")
+       when (item.itemId){
+           R.id.profile -> {
+               navcon.navigate(R.id.action_home1_to_profile)
+               Log.d("tag","success")
+               return true
+           }
+
+           R.id.apperance -> {
+               navcon.navigate(R.id.action_home1_to_apperance2)
+               return true
+           }
+           else -> return false
+
+       }
+    }
+
+
 }
